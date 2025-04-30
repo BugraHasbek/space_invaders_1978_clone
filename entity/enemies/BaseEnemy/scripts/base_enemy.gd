f6e53cc3244d7536de47c25d623334fdfdc4ca71
Hasbey <bugrahasbek@gmail.com>
2025-04-30 14:54:10 +0300

    enemy death animation implmented

--- entity/enemies/BaseEnemy/scripts/base_enemy.gd
+++ entity/enemies/BaseEnemy/scripts/base_enemy.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 signal enemy_died
 
 # Base enemy properties
@@ -10,5 +12,10 @@ func _get_score() -> int :
 	return score_value
 
 func die():
+	sprite.stop()
+	sprite.play("death")
+	sprite.connect("animation_finished", _on_death_animation_finished)
 	emit_signal("enemy_died", score_value)  # Emit the score value when destroyed
+	
+func _on_death_animation_finished() -> void:
 	queue_free()
